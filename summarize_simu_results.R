69bab370612fef4a563a6c4939316420803fee37
qinglong-tian <qltian2021@gmail.com>
2022-09-22 10:54:48 -0500

    add se and cp

--- summarize_simu_results.R
+++ summarize_simu_results.R
@@ -9,9 +9,15 @@ compare_mse <- function(summary_simulation, index)
   mean(sapply(summary_simulation, function(x) {
     x$mse34[index]
   })) -> mse34
+  mean(sapply(summary_simulation, function(x) {
+    x$mse_add[index]
+  })) -> mseadd
+  mean(sapply(summary_simulation, function(x) {
+    x$mse_debias[index]
+  })) -> msedebias
   
-  valVec <- c(mse3, mse4, mse34)
-  names(valVec) <- c("col3", "col4", "comb")
+  valVec <- c(mse3, mse4, mse34, mseadd, msedebias)
+  names(valVec) <- c("m1", "m2", "comb", "additive", "debias")
   
   return(valVec)
 }
@@ -27,8 +33,60 @@ compare_cp <- function(summary_simulation, index)
   mean(sapply(summary_simulation, function(x) {
     x$cp34[index]
   })) -> cp34
+  mean(sapply(summary_simulation, function(x) {
+    x$cp_add[index]
+  })) -> cpadd
+  mean(sapply(summary_simulation, function(x) {
+    x$cp_debias[index]
+  })) -> cpdebias
   
-  cpVec <- c(cp3, cp4, cp34)
-  names(cpVec) <- c("col3", "col4", "comb")
+  cpVec <- c(cp3, cp4, cp34, cpadd, cpdebias)
+  names(cpVec) <- c("col3", "col4", "comb", "additive", "debias")
+  return(cpVec)
+}
+
+compare_cp <- function(summary_simulation, index)
+{
+  mean(sapply(summary_simulation, function(x) {
+    x$cp3[index]
+  })) -> cp3
+  mean(sapply(summary_simulation, function(x) {
+    x$cp4[index]
+  })) -> cp4
+  mean(sapply(summary_simulation, function(x) {
+    x$cp34[index]
+  })) -> cp34
+  mean(sapply(summary_simulation, function(x) {
+    x$cp_add[index]
+  })) -> cpadd
+  mean(sapply(summary_simulation, function(x) {
+    x$cp_debias[index]
+  })) -> cpdebias
+  
+  cpVec <- c(cp3, cp4, cp34, cpadd, cpdebias)
+  names(cpVec) <- c("col3", "col4", "comb", "additive", "debias")
+  return(cpVec)
+}
+
+compare_se <- function(summary_simulation, index)
+{
+  mean(sapply(summary_simulation, function(x) {
+    x$se3[index]
+  })) -> cp3
+  mean(sapply(summary_simulation, function(x) {
+    x$se4[index]
+  })) -> cp4
+  mean(sapply(summary_simulation, function(x) {
+    x$se34[index]
+  })) -> cp34
+  mean(sapply(summary_simulation, function(x) {
+    x$se_add[index]
+  })) -> cpadd
+  mean(sapply(summary_simulation, function(x) {
+    x$se_debias[index]
+  })) -> cpdebias
+  
+  cpVec <- c(cp3, cp4, cp34, cpadd, cpdebias)
+  names(cpVec) <- c("col3", "col4", "comb", "additive", "debias")
   return(cpVec)
 }
