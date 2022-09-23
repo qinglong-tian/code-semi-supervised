63199f3aa4d0def6b612228dc8afb20ba9cc3873
qinglong-tian <qltian2021@gmail.com>
2022-09-23 12:23:04 -0500

    use additive as one of the m functions

--- simu_safe_estimators.R
+++ simu_safe_estimators.R
@@ -4,7 +4,7 @@ source("nec.R")
 setwd("../")
 source("aggregation_aux_functions.R")
 
-B <- 120
+B <- 5
 
 # Factor levels
 n <- 200
@@ -35,12 +35,15 @@ results <- mclapply(data_mc_list, function(data1)
   X <- data1$X
   Ytrue <- scale(data1$y, scale = F)
   y1 <- scale(data1$y[1:n], center = T, scale = F)
-  Xs <- scale(X[1:n, ], scale = F)
+  Xs <- scale(X[1:n,], scale = F)
   Xs2 <- scale(X, scale = F)
   sdxinv <- 1 / sqrt(colSums(Xs ^ 2) / (n - 1))
   
+  # Fit Lasso Using All Columns
+  beta_ll <- Fit_Lasso(y_centered = y1, X_centered = Xs)
+  
   # Debiased Lasso 1
-  object <- lasso.proj(X[c(1:n),], y1, robust = T)
+  object <- lasso.proj(X[c(1:n), ], y1, robust = T)
   beta_ll_debias1 <- object$bhat
   beta_ll_debias1_se <- object$se
   beta_ll_debias1_cp <-
@@ -62,7 +65,7 @@ results <- mclapply(data_mc_list, function(data1)
   for (i in 1:p) {
     index <- c(index, rep(i, df))
   }
-  XX <- bsMat1[1:n,] # Regressor in group lasso
+  XX <- bsMat1[1:n, ] # Regressor in group lasso
   lambda.grp <-
     lambdamax(
       XX,
@@ -94,18 +97,16 @@ results <- mclapply(data_mc_list, function(data1)
     X_centered_all_data = Xs2
   )
   
-  # Fit Lasso Using All Columns
-  beta_ll <- Fit_Lasso(y_centered = y1, X_centered = Xs)
-  
   # Fit Lasso Using the Cubic of the 3rd Column
-  Xm1 <- X[1:n,]
-  Xm1 <- scale(Xm1, scale = F)
-  Xm1[, 3] <- Xm1[, 3] ^ 3
-  
-  Xm1_full <- scale(X, scale = F)
-  Xm1_full[, 3] <- Xm1_full[, 3] ^ 3
+  X_full_m1 <- X
+  X_full_m1[, 3] <- X_full_m1[, 3] ^ 3
+  X_full_m1[, 1] <- X_full_m1[, 1] ^ 2
+  # X_full_m1[, -c(1, 3)] <- 0
+  X_full_m1 <- scale(X_full_m1, scale = F)
+  Xm1 <- X_full_m1[1:n, ]
   beta_ll_m1 <- Fit_Lasso(y_centered = y1, X_centered = Xm1)
-  yAll_m1 <- Xm1_full %*% beta_ll_m1
+  yAll_m1 <- X_full_m1 %*% beta_ll_m1
+  
   zeta_new_m1 <- Compute_Zeta(
     y_centered = y1,
     X_centered = Xs,
@@ -114,30 +115,13 @@ results <- mclapply(data_mc_list, function(data1)
     X_centered_all_data = Xs2
   )
   
-  # Fit Lasso Using the Square the 1th Column
-  Xm2 <- X[1:n,]
-  Xm2 <- scale(Xm2, scale = F)
-  Xm2[, 1] <- Xm2[, 1] ^ 2
-  
-  Xm2_full <- scale(X, scale = F)
-  Xm2_full[, 1] <- Xm2_full[, 1] ^ 2
-  beta_ll_m2 <- Fit_Lasso(y_centered = y1, X_centered = Xm2)
-  yAll_m2 <- Xm2_full %*% beta_ll_m2
-  zeta_new_m2 <- Compute_Zeta(
-    y_centered = y1,
-    X_centered = Xs,
-    beta_lasso_original = beta_ll,
-    predY_m = yAll_m2,
-    X_centered_all_data = Xs2
-  )
-  
   # Combining m1 and m2
   Compute_Zeta_Two_Models(
     y_centered = y1,
     X_centered = Xs,
     beta_lasso_origial = beta_ll,
-    predY_m1 = yAll_m1,
-    predY_m2 = yAll_m2,
+    predY_m1 = Y_all_additive,
+    predY_m2 = yAll_m1,
     X_centered_all_data = Xs2
   ) -> zeta_new_two_models_list
   
@@ -158,7 +142,7 @@ results <- mclapply(data_mc_list, function(data1)
     beta_ll = beta_ll,
     alpha = alpha,
     beta_t = beta_t
-  ) -> est_inf_34
+  ) -> est_inf_comb
   
   ### m1
   Estimate_Inference_Safe_Estimator(
@@ -174,22 +158,7 @@ results <- mclapply(data_mc_list, function(data1)
     alpha = alpha,
     beta_t = beta_t
   ) -> est_inf_m1
-  
-  ### m2
-  Estimate_Inference_Safe_Estimator(
-    X = X,
-    y1 = y1,
-    zeta_list = zeta_new_m2,
-    ratio = ratio,
-    Xs = Xs,
-    omegaHat = omegaHat,
-    sdxinv = sdxinv,
-    n = n,
-    beta_ll = beta_ll,
-    alpha = alpha,
-    beta_t = beta_t
-  ) -> est_inf_m2
-  
+
   ### additive
   Estimate_Inference_Safe_Estimator(
     X = X,
@@ -207,8 +176,7 @@ results <- mclapply(data_mc_list, function(data1)
   
   return(list(
     rmv3 = est_inf_m1,
-    rmv4 = est_inf_m2,
-    rmv34 = est_inf_34,
+    rmv34 = est_inf_comb,
     add = est_inf_add,
     debias_lasso = debias_lasso
   ))
